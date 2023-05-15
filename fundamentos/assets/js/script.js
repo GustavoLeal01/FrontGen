a43b5d3a83c03027834dc369731851fb521e6a7e
Gustavoleal1 <goialeal20@gmail.com>
2023-05-15 19:16:38 -0300

    teste em CSS E JavaScript

---
+++ fundamentos/assets/js/script.js
@@ -0,0 +1,12 @@
+let nome = window.document.getElementById("nome")
+let email = document.querySelector("email")
+let assunto = document.querySelector("assunto")
+
+function validaNome() {
+    
+    if (nome.ariaValueMax.length < 3) {
+alert("nome inválido")
+    }
+    
+
+}
