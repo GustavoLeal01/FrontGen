dd20ac134b7512168874518fe883937733916e4a
Gustavo Leal <123138088+GustavoLeal01@users.noreply.github.com>
2023-05-16 11:17:18 -0300

    testes  script.js

--- fundamentos/assets/js/script.js
+++ fundamentos/assets/js/script.js
@@ -2,6 +2,10 @@ let nome = window.document.getElementById("nome")
 let email = document.querySelector("email")
 let assunto = document.querySelector("assunto")
 
+nome.style.width = "100%"
+email.style.width = "100%"
+
+
 function validaNome() {
     
     if (nome.ariaValueMax.length < 3) {
